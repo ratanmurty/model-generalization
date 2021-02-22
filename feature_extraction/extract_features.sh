133d6abdae5cee50b95d2ea5d9d08a672d685106
Isha Ghodgaonkar <ighodgao@korbinian.ib.cluster>
2021-02-22 12:53:55 -0500

    removing redundancies

--- feature_extraction/extract_features.sh
+++ feature_extraction/extract_features.sh
@@ -3,10 +3,6 @@
 #SBATCH -t 72:0:0
 
 tf_slim_path=/mindhive/nklab4/users/ighodgao
-script_path=.
-
-source ~/.bashrc
 conda activate "brain-score"
 export PYTHONPATH="$PYTHONPATH:$tf_slim_path/tf-models/research/slim"
-cd $script_path
 python3 extract_features.py --images_path=$1 --model=$2 --save_dir=$3 --index=$4
